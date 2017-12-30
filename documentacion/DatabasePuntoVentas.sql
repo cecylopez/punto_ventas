d539c730d3f158f1e4ec377ee7b517de5f909533
cecylopez <ceciliac86548@gmail.com>
2017-12-30 00:33:04 -0600

    cambios en la base de datos, creacion de las foreign keys y cambios a la llave primaria

--- documentacion/DatabasePuntoVentas.sql
+++ documentacion/DatabasePuntoVentas.sql
@@ -1,24 +1,24 @@
 
 
 CREATE TABLE rol (
-idRol INT SERIAL PRIMARY KEY NOT NULL,
+idRol SERIAL PRIMARY KEY NOT NULL,
 nombre VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE usuario (
-idUsuario INT SERIAL PRIMARY KEY NOT NULL,
+idUsuario SERIAL PRIMARY KEY NOT NULL,
 nombre VARCHAR(45) NOT NULL,
 idrol INT NOT NULL
 );
 
 CREATE TABLE categoria (
-idCategoria  INT  SERIAL PRIMARY KEY NOT NULL,
+idCategoria SERIAL PRIMARY KEY NOT NULL,
 nombre VARCHAR(45) NOT NULL,
 imagen VARCHAR(300)
 );
 
 CREATE TABLE producto (
-idProducto INT SERIAL PRIMARY KEY NOT NULL,
+idProducto SERIAL PRIMARY KEY NOT NULL,
 nombre VARCHAR(45) NOT NULL,
 precio DECIMAL NOT NULL,
 imagen VARCHAR(300),
@@ -26,17 +26,17 @@ idCategoria INT NOT NULL
 );
 
 CREATE TABLE sucursal (
-idSucursal INT SERIAL PRIMARY KEY NOT NULL,
+idSucursal SERIAL PRIMARY KEY NOT NULL,
 nombre VARCHAR(45) NOT NULL,
 ubicacion VARCHAR(45)
 );
 
 CREATE TABLE venta (
-idVenta INT SERIAL PRIMARY KEY NOT NULL,
+idVenta SERIAL PRIMARY KEY NOT NULL,
 identificadorcliente CHAR NOT NULL,
 tipoVenta CHAR NOT NULL,
-fechaCreacion DATETIME NOT NULL,
-fechaPego DATETIME NOT NULL,
+fechaCreacion TIMESTAMP NOT NULL,
+fechaPego TIMESTAMP NOT NULL,
 status CHAR NOT NULL,
 tipoPago CHAR,
 montoTotal DECIMAL,
@@ -47,10 +47,38 @@ idUsuario INT NOT NULL
 );
 
 CREATE TABLE detalle (
-idDetalle INT SERIAL PRIMARY KEY NOT NULL,
+idDetalle SERIAL PRIMARY KEY NOT NULL,
 idVenta INT NOT NULL,
 idProducto INT NOT NULL,
 cantidad DECIMAL NOT NULL
 );
 
-ALTER TABLE
+ALTER TABLE "usuario"
+   ADD CONSTRAINT fk_idRol
+   FOREIGN KEY (idRol)
+   REFERENCES "rol"(idRol);
+
+ALTER TABLE "producto"
+   ADD CONSTRAINT fk_idCategoria
+   FOREIGN KEY (idCategoria)
+   REFERENCES "categoria"(idCategoria);
+
+ALTER TABLE "venta"
+   ADD CONSTRAINT fk_idUsuario
+   FOREIGN KEY (idUsuario)
+   REFERENCES "usuario"(idUsuario);
+
+ALTER TABLE "venta"
+   ADD CONSTRAINT fk_idSucursal
+   FOREIGN KEY (idSucursal)
+   REFERENCES "sucursal"(idSucursal);
+
+ALTER TABLE "detalle"
+   ADD CONSTRAINT fk_idVenta
+   FOREIGN KEY (idVenta)
+   REFERENCES "venta"(idVenta);
+
+ALTER TABLE "detalle"
+   ADD CONSTRAINT fk_idProducto
+   FOREIGN KEY (idProducto)
+   REFERENCES "producto"(idProducto);
